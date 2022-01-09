d094a0912928bc625a62d02af3c89bc455654338
OkanUysal <uysal.okan.2010@gmail.com>
2022-01-09 15:50:11 +0300

    A simple website was created.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import InfoPanel from './components/info-panel/InfoPanel';
+import DetailTabPanel from './components/detail-tabpanel/DetailTabPanel';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload. Try to change.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <InfoPanel></InfoPanel>
+                <DetailTabPanel></DetailTabPanel>
+            </div>
+        );
+    }
 }
 
 export default App;
