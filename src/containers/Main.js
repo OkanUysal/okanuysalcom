3b77f2d15079cbadb730b59f5e0cc121d6943628
OkanUysal <uysal.okan.2010@gmail.com>
2022-01-12 19:37:23 +0300

    warning fix

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -6,7 +6,6 @@ import Experience from "../pages/experience/Experience";
 import Opensource from "../pages/opensource/Opensource";
 import Contact from "../pages/contact/ContactComponent";
 import Projects from "../pages/projects/Projects";
-import { settings } from "../portfolio.js";
 import Error404 from "../pages/errors/error404/Error";
 
 export default class Main extends Component {
@@ -55,6 +54,12 @@ export default class Main extends Component {
               path="/projects"
               render={(props) => (
                 <Projects {...props} theme={this.props.theme} />
+              )}
+            />
+            <Route
+              path="*"
+              render={(props) => (
+                <Error404 {...props} theme={this.props.theme} />
               )}
             />
           </Switch>
